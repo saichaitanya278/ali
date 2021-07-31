ce79c142c2a45bd944951863a4f29cfae1a93001
saichaitanya <62346436+saichaitanya278@users.noreply.github.com>
2021-07-31 15:43:44 +0530

    Update all.sh

--- all.sh
+++ all.sh
@@ -2,7 +2,7 @@ sudo pacman -S --needed base-devel -y
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
-paru -S xf86-video-amdgpu xorg  pcmanfm  sxiv alacritty xfce4 celluloid nerd-fonts-mononoki lightdm lightdm-webkit-theme-aether lightdm-webkit2-greeter neofetch -y
+paru -S xf86-video-amdgpu xorg  pcmanfm  sxiv alacritty xfce4 celluloid nerd-fonts-mononoki lightdm lightdm-webkit-theme-aether lightdm-webkit2-greeter neofetch exfat-utils autofs fuse2 fuse3 fuse fuseiso xorg-server xorg-xinit font-bh-ttf  gsfonts sdl_ttf ttf-bitstream-vera ttf-dejavu ttf-liberation xorg-fonts-type1  ttf-indic-otf  noto-fonts-emoji font-bh-ttf  sdl_ttf ttf-bitstream-vera ttf-dejavu ttf-liberation xorg-fonts-type1 ntfs-3g  alsa-utils alsa-plugins  pulseaudio pulseaudio-alsa  unrar  zip unzip p7zip gvfs-mtp  jmtpfs  mtpfs  xdg-user-dirs -y
 #paru -S brave-bin telegram-desktop-bin terminator qbittorrent file-roller light net-tools  flatpak virtualbox pavucontrol rhythmbox  freeoffice  qtile rofi nitrogen picom pcmanfm lxsession sxiv alacritty xfce4-power-manager  -y
 sudo systemctl enable lightdm
 #git clone https://gitlab.com/saichaitanya278/dotfiles.git  
