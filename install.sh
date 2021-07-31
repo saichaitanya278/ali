caacfa83814244d87d7e43f5bb3dad4ba800a9dc
saichaitanya <62346436+saichaitanya278@users.noreply.github.com>
2021-07-31 09:49:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
+iwctl
+station wlan0 scan
+station wlan0 get-networks
+station wlan0 connect "Sai Chaitanya 5Ghz"
 timedatectl set-ntp true
-mkfs.fat -F32 /dev/nvme0n1p5
-mkfs.ext4 /dev/nvme0n1p7
-mkswap /dev/nvme0n1p6
-swapon /dev/nvme0n1p6
-mount /dev/nvme0n1p7 /mnt
-mount /dev/nvme0n1p5 /mnt/boot/
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4 /dev/sda3
+mkswap /dev/sda2
+swapon /dev/sda2
+mount /dev/sda3 /mnt
+mmkdir /mnt/boot
+mount /dev/sda1 /mnt/boot/
 pacstrap /mnt base linux linux-firmware nano grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant dialog os-prober mtools dosfstools base-devel linux-headers reflector grub-btrfs
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
