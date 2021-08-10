b14b6f4ee8aba66652d929edbddc13dd407ce337
saichaitanya <62346436+saichaitanya278@users.noreply.github.com>
2021-08-10 09:59:09 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -3,7 +3,7 @@
 #station wlan0 get-networks
 #station wlan0 connect "Sai Chaitanya 5Ghz"
 timedatectl set-ntp true
-:'ssd drive
+: ' ssd drive
 mkfs.fat -F32 /dev/nvme0n1p5
 mkfs.ext4 /dev/nvme0n1p7
 mkswap /dev/nvme0n1p6
@@ -12,6 +12,7 @@ mount /dev/nvme0n1p7 /mnt
 mkdir /mnt/boot
 mount /dev/nvme0n1p5 /mnt/boot/
 '
+: ' hdd drive
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda3
 mkswap /dev/sda2
@@ -19,6 +20,11 @@ swapon /dev/sda2
 mount /dev/sda3 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot/
+'
+: ' dos
+mkfs.ext4 /dev/sda1
+mount /dev/sda1 /mnt
+'
 pacstrap /mnt base linux-lts linux-firmware nano grub efibootmgr vim networkmanager git network-manager-applet wireless_tools wpa_supplicant dialog os-prober mtools dosfstools base-devel linux-lts-headers reflector
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
