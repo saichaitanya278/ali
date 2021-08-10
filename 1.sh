52e997a9596c9f9f099594ddef020a75548657e2
saichaitanya <62346436+saichaitanya278@users.noreply.github.com>
2021-08-10 09:33:14 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -3,6 +3,15 @@
 #station wlan0 get-networks
 #station wlan0 connect "Sai Chaitanya 5Ghz"
 timedatectl set-ntp true
+:'ssd drive
+mkfs.fat -F32 /dev/nvme0n1p5
+mkfs.ext4 /dev/nvme0n1p7
+mkswap /dev/nvme0n1p6
+swapon /dev/nvme0n1p6
+mount /dev/nvme0n1p7 /mnt
+mkdir /mnt/boot
+mount /dev/nvme0n1p5 /mnt/boot/
+'
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda3
 mkswap /dev/sda2
