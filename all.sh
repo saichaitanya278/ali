bf1849ede1c3dc2cb9af632c2405d20b26169d98
saichaitanya <62346436+saichaitanya278@users.noreply.github.com>
2021-10-06 16:17:14 +0530

    Update all.sh

--- all.sh
+++ all.sh
@@ -7,10 +7,18 @@ paru -S exfat-utils autofs fuse2 fuse3 fuse fuseiso xorg-server xorg-xinit font-
 paru -S lightdm lightdm-webkit-theme-aether lightdm-webkit2-greeter xorg 
 sudo systemctl enable lightdm
 : ' virtualbox
-paru -S xf86-video-vmware  terminator neofetch sxiv celluloid pcmanfm nerd-fonts-mononoki file-roller net-tools 
+paru -S git xf86-video-vmware neofetch sxiv pcmanfm nerd-fonts-mononoki file-roller mpv net-tools  xmonad xmonad-contrib xfce4-settings xdotool  alacritty  xmobar  trayer  nitrogen picom pcmanfm lxsession sxiv xfce4-power-manager  flameshot  volumeicon pavucontrol papirus-icon-theme  light exa otf-font-awesome-5-free htop iftop lxappearance dmenu materia-theme xfce4-clipman-plugin -y
+curl -L http://get.oh-my.fish | fish
+paru -S starship qt5ct kvantum-qt5
+git clone https://gitlab.com/saichaitanya278/dotfiles.git  
+cd ~
+cp -r -v dotfiles/xmonad/ ~/
+cp -r -v dotfiles/.config/xmobar/ ~/.config/
+cp -r -v dotfiles/.config/alacritty/ ~/.config/
+cp -r -v dotfiles/.config/fish/ ~/.config/
 '
 : ' my host
-paru -S xf86-video-amdgpu  xmonad xmonad-contrib  xdotool  alacritty  xmobar  trayer  nitrogen picom pcmanfm lxsession sxiv xfce4-power-manager  flameshot  volumeicon pavucontrol papirus-icon-theme  light exa otf-font-awesome-5-free htop iftop lxappearance dmenu materia-theme xfce4-clipman-plugin -y
+paru -S xf86-video-amdgpu  xmonad xmonad-contrib xfce4-settings xdotool  alacritty  xmobar  trayer  nitrogen picom pcmanfm lxsession sxiv xfce4-power-manager  flameshot  volumeicon pavucontrol papirus-icon-theme  light exa otf-font-awesome-5-free htop iftop lxappearance dmenu materia-theme xfce4-clipman-plugin -y
 paru -S brave-bin telegram-desktop-bin  light  virtualbox pavucontrol rhythmbox mpv neofetch sxiv nerd-fonts-mononoki file-roller net-tools -y
 sudo pacman -S nvidia nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader nvidia nvidia-dkms nvidia-prime lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader -y
 curl -L http://get.oh-my.fish | fish
